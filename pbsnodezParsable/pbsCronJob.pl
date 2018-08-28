28c590203908c54bf1e0542b8295cb8a5db42c45
zackramjan <zack.hr@gmail.com>
2018-08-28 14:30:20 -0400

    moved multiple cron job entries into a single cron job that has file-size error checking

---
+++ pbsnodezParsable/pbsCronJob.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+
+
+system("bash -l -c /primary/vari/admin/tools/pbsnodezParsable/pbsnodezParsable > /var/www/html/pbsnodezTxt.txt.new");
+system("bash -l -c \"qstat -a | tail -n +6\" > /var/www/html/qstat.txt.new");
+system("cp /primary/vari/admin/tools/quotaGraph/quotatable.txt /var/www/html/quotatable.txt");
+
+checkAndReplace("/var/www/html/pbsnodezTxt.txt.new","/var/www/html/pbsnodezTxt.txt");
+checkAndReplace("/var/www/html/qstat.txt.new","/var/www/html/qstat.txt");
+
+sub checkAndReplace()
+{
+  my $src = shift @_;
+  my $dst = shift @_;
+  if (-s $src > 2000)
+  { 
+    system("mv $src $dst")
+  }
+}

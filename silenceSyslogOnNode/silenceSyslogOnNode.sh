20b5c4a8b74f877604bfea39fb574eadff48b217
zackramjan <zack.hr@gmail.com>
2018-10-15 14:50:27 -0400

    added syslog restart after change

--- silenceSyslogOnNode/silenceSyslogOnNode.sh
+++ silenceSyslogOnNode/silenceSyslogOnNode.sh
@@ -2,6 +2,7 @@ if [  ${#1} -gt 4 ]
 then
   echo silencing rsyslog broadcast on $1
   ssh $1 /primary/vari/admin/tools/silenceSyslogOnNode/silenceSyslogOnNode.sh  
+  ssh $1 systemctl restart rsyslog
 fi
 if [ $# -eq 0 ]
 then
--- silenceSyslogOnNode/unsilenceSyslogOnNode.sh
+++ silenceSyslogOnNode/unsilenceSyslogOnNode.sh
@@ -2,6 +2,7 @@ if [  ${#1} -gt 4 ]
 then
   echo unsilencing rsyslog broadcast on $1
   ssh $1 /primary/vari/admin/tools/silenceSyslogOnNode/unsilenceSyslogOnNode.sh  
+  ssh $1 systemctl restart rsyslog
 fi
 if [ $# -eq 0 ]
 then

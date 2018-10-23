cb7249c82f311f36b393691d58d74a901786e7bb
zackramjan <zack.hr@gmail.com>
2018-10-22 21:56:01 -0400

    updated quota dump to use mmrepquota for all filesets at once

--- quotaGraph/quotagraph.pl
+++ quotaGraph/quotagraph.pl
@@ -14,7 +14,8 @@
 $outfile = "/primary/vari/admin/tools/quotaGraph/quotatable.txt";
 $gpfs = "/usr/lpp/mmfs/bin";
 
-$out = `for i in \`$gpfs/mmlsfileset  home | cut -f 1 -d ' ' | tail -n +3 | sort | xargs \`; do echo \$i \`$gpfs/mmlsquota -j \$i home   | tail -n 1 | sed 's/|.\+//g' \` ; done ; for i in \`$gpfs/mmlsfileset  scratch | cut -f 1 -d ' ' | tail -n +3 | sort | xargs \`; do echo \$i \`$gpfs/mmlsquota -j \$i scratch  | tail -n 1 | sed 's/|.\+//g' \` ; done `;
+$out = `$gpfs/mmrepquota -j home | tail -n +4 | sed 's/root\\s*FILESET/home FILESET/' | sed 's/\\s\\s*/ /g'`;
+$out .=`$gpfs/mmrepquota -j scratch | tail -n +4 | sed 's/root\\s*FILESET/scratch FILESET/' | sed 's/\\s\\s*/ /g'`;
 
 open OUT, ">$outfile";
 print OUT $out;

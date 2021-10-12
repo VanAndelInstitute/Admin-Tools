5c4b5506ff9dcf8ca53cece6a0c5c9db38d83862
zackramjan <zack.hr@gmail.com>
2021-10-12 11:29:50 -0400

    created tool to watch interface throughput without needing external tools. usage: watchbw.pl interface_name

---
+++ bwmonitor/watchbw.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+$if = $ARGV[0] || die "specify a network interface";
+$sleep = $ARGV[1] || 10;
+
+$rx = "ifconfig $if | grep RX | grep pack";
+$tx = "ifconfig $if | grep TX |grep pack";
+
+print STDERR "watching $if for $sleep seconds...\n";
+
+while (1)
+{
+	my $rx1 = &getBytes(`$rx`);
+	my $tx1 = &getBytes(`$tx`);
+	sleep $sleep;
+	my $rx2 = &getBytes(`$rx`);
+	my $tx2 = &getBytes(`$tx`);
+	print "RX: " . (($rx2 - $rx1)/$sleep/1000000) . "\tMBs\t\t";
+	print "TX: " . (($tx2 - $tx1)/$sleep/1000000) . "\tMBs\n";
+
+}
+
+
+
+sub getBytes
+{
+	my $s = shift @_;
+	$s =~ /bytes\s+(\d+)\s+/;
+	return $1;
+}
